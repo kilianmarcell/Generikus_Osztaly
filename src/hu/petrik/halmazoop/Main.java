05fe78ed95e6c5c55ff545ca650ef2c5704a69ee
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-10 16:27:52 +0100

    Busz kiírva

--- src/hu/petrik/halmazoop/Main.java
+++ src/hu/petrik/halmazoop/Main.java
@@ -1,12 +1,15 @@
 package hu.petrik.halmazoop;
 
 import hu.petrik.Exercise.SzobaKerekpar;
+import hu.petrik.busz.Buszok;
 
 public class Main {
 
     public static void main(String[] args) {
 
         SzobaKerekpar.Run();
+        System.out.println();
+        Buszok.Run();
 
     }
 }
