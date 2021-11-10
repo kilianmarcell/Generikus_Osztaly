6bdae11df90861d22ea570fe4d2f1f20db9da830
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-10 15:52:23 +0100

    Funkciók létrehozva

--- src/hu/petrik/Set/Set.java
+++ src/hu/petrik/Set/Set.java
@@ -52,4 +52,97 @@ public class Set<T> { // T tipusú elemek fogadására alkalmas
 
     }
 
+    public boolean isElement(T element) {
+
+        int i = 0;
+
+        while (i < this.elements.size() && this.elements.get(i) != element) {
+
+            i++;
+
+        }
+
+        //this.elements.contains(element);
+        return i < this.elements.size();
+
+    }
+
+    public boolean isEmpty() {
+
+        //this.elements.isEmpty();
+        return 0 == this.elements.size();
+
+    }
+
+    public boolean isEquals(Set<T> otherSet) {
+
+        return this.getSize() == otherSet.getSize() && this.isPartOf(otherSet);
+
+    }
+
+    public boolean isPartOf(Set<T> otherSet) {
+
+        boolean isBigger = this.getSize() > otherSet.getSize();
+        boolean isPartOf = false;
+
+        if (!isBigger) {
+
+            int i = 0;
+
+            while (i < this.getSize() && otherSet.isElement(this.elements.get(i))) {
+
+                i++;
+
+            }
+
+            isPartOf = i == this.getSize();
+
+        }
+
+        return isPartOf;
+
+    }
+
+    public Set<T> unio(Set<T> otherSet) {
+
+        Set<T> unioSet = new Set<>();
+
+        for (T element : this.elements) {
+
+            unioSet.add(element);
+
+        }
+
+        for (T element : otherSet.elements) {
+
+            unioSet.add(element);
+
+        }
+
+        return unioSet;
+
+    }
+
+    public Set<T> intersect(Set<T> otherSet) {
+
+        Set<T> intersect = new Set<>();
+
+        for (T i : intersect.elements) {
+
+            for (T f : otherSet.elements) {
+
+             if (i == f) {
+
+                 intersect.add(i);
+
+             }
+
+            }
+
+        }
+
+        return intersect;
+
+    }
+
 }
