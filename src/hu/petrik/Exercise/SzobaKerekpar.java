f6f6aae58658817f59b05609eccc3104b34a05aa
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-10 15:59:46 +0100

    Feleség és férj értékek hozzáadva

---
+++ src/hu/petrik/Exercise/SzobaKerekpar.java
@@ -0,0 +1,46 @@
+package hu.petrik.Exercise;
+
+import hu.petrik.Set.Set;
+
+public class SzobaKerekpar {
+
+    private Set<Integer> feleseg;
+    private Set<Integer> ferj;
+
+    public SzobaKerekpar() {
+
+        this.feleseg = new Set<>();
+
+            feleseg.add(1);
+            feleseg.add(3);
+            feleseg.add(4);
+            feleseg.add(5);
+            feleseg.add(7);
+            feleseg.add(8);
+            feleseg.add(9);
+            feleseg.add(10);
+            feleseg.add(14);
+            feleseg.add(17);
+            feleseg.add(18);
+            feleseg.add(19);
+            feleseg.add(20);
+            feleseg.add(24);
+            feleseg.add(27);
+
+        this.ferj = new Set<>();
+
+            ferj.add(7);
+            ferj.add(13);
+            ferj.add(14);
+            ferj.add(15);
+            ferj.add(20);
+            ferj.add(21);
+            ferj.add(23);
+            ferj.add(24);
+            ferj.add(25);
+            ferj.add(29);
+            ferj.add(30);
+
+    }
+
+}
--- src/hu/petrik/Exercise/SzobaKerékpár.java
+++
@@ -1,9 +0,0 @@
-package hu.petrik.Exercise;
-
-import hu.petrik.Set.Set;
-
-public class SzobaKerékpár {
-
-    private Set<Integer>
-
-}
